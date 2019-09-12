41f81bf8b12e64b53cb87a577154d4a04c0cdbe4
Ubuntu <ubuntu@ip-172-31-21-52.ec2.internal>
2019-09-12 23:21:34 +0000

    made code easier to understad

---
+++ hello_world3.py
@@ -0,0 +1,13 @@
+#!usr/bin/env python3
+
+# Ceated by: Cameron Teed
+# Created On: September 2019
+# This is the "Hello_World" Program, but pretty
+
+
+def main():
+    print("Hello, World")
+
+
+if __name__ == "__main__":
+    main()
